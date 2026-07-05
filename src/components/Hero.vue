d4d63c13011e9b0ae9555c1180d5b93f367dd8cb
Renan-G-projec <renanfartes@gmail.com>
2026-07-05 20:00:28 -0300

    add: Hero PFP added. AMDG

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -5,7 +5,7 @@
 
     const defaultString = "Just a beginner JS and C/C++ student";
     const showedString = ref("");
-    const delayBetween = 70;
+    const delayBetween = 40;
     let charactersNumber = 0;
     
     function typewritterStep() {
@@ -25,5 +25,51 @@
 </script>
 
 <template>
-    <h1>{{ showedString }}</h1>
+    <div class="hero">
+        <div class="content">
+            <h1>Renan Gabriel</h1>
+            <h2>{{ showedString }}</h2>
+        </div>
+
+        <div class="pfp-container">
+            <img src="/public/profileImage.jpg" alt="My pfp">
+        </div>
+
+    </div>
 </template>
+
+<style scoped>
+    @import url('https://fonts.googleapis.com/css2?family=Passero+One&display=swap');
+
+    .hero {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+    }
+
+    h1, h2 {
+        margin: 0;
+
+        font-family: "Passero One", sans-serif;
+        font-style: normal;
+    }
+    h1 {
+        font-size: 7rem;
+    }
+
+    .pfp-container {
+        overflow: hidden;
+
+        border-radius: 50%;
+    }
+
+    img {
+        height: 14rem;
+        width: 14rem;
+
+        object-fit: cover;
+
+        transform: scale(1.1);
+        transform-origin: 50% 50%;
+    }
+</style>
