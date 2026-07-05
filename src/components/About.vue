d1ac3603003ea057f5b1b27421b53454abc3a5f0
Renan-G-projec <renanfartes@gmail.com>
2026-07-05 20:30:27 -0300

    add: AboutMe section content added. AMDG

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<!-- Ad Maiorem Dei Gloriam! -->
+
+<template>
+    <h2>About Me</h2>
+    <p>
+        I am a 15 year old programmer from Brazil. 
+        I live in the north of <em>Santa Catarina</em> 
+        and i love code since i was 10.
+        My goal is to study abroad , work as a
+        software engineer and help the world to become
+        a better place. I'm also passionated about 
+        the universe, the galaxies and mechanic enginering.
+        I love understanding how things work by inside, and 
+        i'm a Roman Catholic.
+    </p>
+</template>
