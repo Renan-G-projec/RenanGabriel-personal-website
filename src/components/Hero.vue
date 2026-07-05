9cffc61b619857fe94a5c504e16366e6033dcdbc
Renan-G-projec <renanfartes@gmail.com>
2026-07-05 19:42:04 -0300

    add: Hero with a typewritter effect!!! AMDG

---
+++ src/components/Hero.vue
@@ -0,0 +1,29 @@
+<!-- Ad Maiorem Dei Gloriam! -->
+
+<script setup>
+    import { onMounted, ref } from 'vue';
+
+    const defaultString = "Just a beginner JS and C/C++ student";
+    const showedString = ref("");
+    const delayBetween = 70;
+    let charactersNumber = 0;
+    
+    function typewritterStep() {
+        showedString.value += defaultString[charactersNumber];
+        charactersNumber++;
+    }
+
+    onMounted(() => {
+        const interval = setInterval(() => {
+            if (charactersNumber < defaultString.length) {
+                typewritterStep();
+            } else {
+                clearInterval(interval);
+            }
+        }, delayBetween)
+    })
+</script>
+
+<template>
+    <h1>{{ showedString }}</h1>
+</template>
