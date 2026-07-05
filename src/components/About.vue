2185624f2b96140158932b433018af88c7f9e561
Renan-G-projec <renanfartes@gmail.com>
2026-07-05 20:45:28 -0300

    add: Image scaling animation added. AMDG

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,22 +1,54 @@
 <!-- Ad Maiorem Dei Gloriam! -->
 
 <template>
-    <h2>About Me</h2>
-    <p>
-        I am a 15 year old programmer from Brazil. 
-        I live in the north of <em>Santa Catarina</em> 
-        and i love code since i was 10.
-        My goal is to study abroad , work as a
-        software engineer and help the world to become
-        a better place. I'm also passionated about 
-        the universe, the galaxies and mechanic enginering.
-        I love understanding how things work by inside, and 
-        i'm a Roman Catholic.
-    </p>
-
-    <img src="../assets/img/catCode.jpg" alt="A cat with a qr code">
+    <div class="about">
+        <div class="content">
+            <h2>About Me</h2>
+            <p>
+                I am a 15 year old programmer from Brazil. 
+                I live in the north of <em>Santa Catarina</em> 
+                and i love code since i was 10.
+                My goal is to study abroad , work as a
+                software engineer and help the world to become
+                a better place. I'm also passionated about 
+                the universe, the galaxies and mechanic enginering.
+                I love understanding how things work by inside, and 
+                i'm a Roman Catholic.
+            </p>
+        </div>
+        
+        <div class="img-container">
+            <img src="../assets/img/catCode.jpg" alt="A cat with a qr code">
+        </div>
+    </div>
 </template>
 
 <style scoped>
+    .about {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        margin-top: 4rem;
+    }
 
+    .content {
+        width: clamp(20rem, 60vw, 100vw);
+    }
+    
+    .img-container {
+        overflow: hidden;
+        border-radius: 1rem;
+        cursor: pointer;
+    }
+    
+    img {
+        height: 14rem;
+        scale: 1.1;
+        transition: transform 0.3s ease-in-out;
+    }
+
+    img:hover {
+        transform: scale(1.2);
+
+    }
 </style>
