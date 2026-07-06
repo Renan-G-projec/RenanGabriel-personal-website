bbec3f4c446cc5996fe4be32a734fc1c953d39a9
Renan-G-projec <renanfartes@gmail.com>
2026-07-06 08:07:58 -0300

    add: Projects content added. AMDG

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
   import Hero from "./components/Hero.vue";
   import About from "./components/About.vue";
+  import Projects from "./components/Projects.vue";
 </script>
 
 <template>
   <Hero/>
   <About/>
+  <Projects/>
 </template>
 
 <style scoped></style>
---
+++ src/components/Projects.vue
@@ -0,0 +1,47 @@
+<!-- Ad Maiorem Dei Gloriam! -->
+
+<script setup>
+    const Projects = [
+        {
+            title: "RenanG_OS",
+            description: "A small browser-based Operational System Simulation!",
+            link: "https://renang-os.netlify.app",
+            github: "https://github.com/Renan-G-projec/RenanG_OS",
+            img: ""
+        }, 
+        {
+            title: "AsciiBird", 
+            description: "A flappy bird game that runs entirely on the terminal!",
+            link: "https://github.com/Renan-G-projec/AsciiBird/releases/tag/v1.0.0",
+            github: "https://github.com/Renan-G-projec/AsciiBird",
+            img: ""
+        },
+        {
+            title: "ScopeCutter",
+            description: "A tool, that helps developers prevent overengineering.",
+            link: "https://scopecutter.netlify.app/",
+            github: "https://github.com/Renan-G-projec/ScopeCutter",
+            img: ""
+        },
+        {
+            title: "WebLinuxTerminal",
+            description: "A web-based unix-like terminal emulation to help new developers learn to work with it!",
+            link: "https://github.com/Renan-G-projec/WebLinuxTerminal",
+            github: "https://github.com/Renan-G-projec/WebLinuxTerminal",
+            img: ""
+        },
+        {
+            title: "Organizer",
+            description: "CLI script that organizes files by their metadata in folders.",
+            link: "https://github.com/Renan-G-projec/Organizer",
+            github: "https://github.com/Renan-G-projec/Organizer",
+            img: ""
+        }
+    ]
+</script>
+
+<template>
+    <div class="projects">
+
+    </div>
+</template>
